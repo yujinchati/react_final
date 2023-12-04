e6791340fb2ed1aa9e68d12a384a81c2854e116f
yujin <yujin.chati@gmail.com>
2023-12-04 10:27:06 +0900

    1204 footer icon 임시삭제

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import './Footer.scss';
-import { FaYoutubeSquare, FaInstagramSquare, FaFacebookSquare, FaTwitterSquare } from 'react-icons/fa';
 
 export default function Footer() {
 	return (
@@ -7,28 +6,6 @@ export default function Footer() {
 			<div className='inner'>
 				<h2>Retreat</h2>
 				<small class='txt_copyright'>Copyright ©Retreat Corp. All rights reserved.</small>
-				<ul>
-					<li>
-						<a href='#' target='_blank'>
-							<FaYoutubeSquare size='20' color='#36363D' />
-						</a>
-					</li>
-					<li>
-						<a href='#' target='_blank'>
-							<FaInstagramSquare size='20' color='#36363D' />
-						</a>
-					</li>
-					<li>
-						<a href='#' target='_blank'>
-							<FaFacebookSquare size='20' color='#36363D' />
-						</a>
-					</li>
-					<li>
-						<a href='#' target='_blank'>
-							<FaTwitterSquare size='20' color='#36363D' />
-						</a>
-					</li>
-				</ul>
 			</div>
 		</div>
 	);
