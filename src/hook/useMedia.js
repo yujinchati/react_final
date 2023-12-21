631876f2c29b380e09670b88675f25920fd0efec
yujin <yujin.chati@gmail.com>
2023-12-22 00:26:57 +0900

    1221 반응형

--- src/hook/useMedia.js
+++ src/hook/useMedia.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 export const useMedia = (opt) => {
-	const defOpt = { mobile: 640, tablet: 1000, laptop: 1400 };
+	const defOpt = { mobile480: 480, mobile: 640, tablet: 1000, laptop: 1400 };
 	const result = { ...defOpt, ...opt };
 	const [Type, setType] = useState('');
 
@@ -10,7 +10,8 @@ export const useMedia = (opt) => {
 		if (wid >= result.laptop) setType('');
 		if (wid >= result.tablet && wid < result.laptop) setType('laptop');
 		if (wid >= result.mobile && wid < result.tablet) setType('tablet');
-		if (wid >= 0 && wid < result.mobile) setType('mobile');
+		if (wid >= result.mobile && wid < result.mobile) setType('mobile');
+		if (wid >= 0 && wid < result.mobile480) setType('mobile mobile480');
 	};
 
 	useEffect(() => {
