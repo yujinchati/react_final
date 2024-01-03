1a1a71bb5a31f3d9880facaf692d3c679ff9ef76
yujin <yujin.chati@gmail.com>
2024-01-03 17:01:19 +0900

    community

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,63 +1,110 @@
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Community.scss';
 
 export default function Community() {
+	const refTit = useRef(null);
+	const refCon = useRef(null);
+
+	const getLocalData = () => {
+		const data = localStorage.getItem('post');
+		if (data) return JSON.parse(data);
+		else return [];
+	};
+	const [Post, setPost] = useState(getLocalData);
+	const creatPost = () => {
+		if (!refTit.current.value.trim() || !refCon.current.value.trim()) {
+			resetPost();
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
+		setPost([{ title: refTit.current.value, content: refCon.current.value, date: new Date(korTime) }, ...Post]);
+		resetPost();
+	};
+	const resetPost = () => {
+		refTit.current.value = '';
+		refCon.current.value = '';
+	};
+	const deletePost = (delIndex) => {
+		//기존 map과 마찬가지로 기존 배열값을 deep copy해서 새로운배열 반환
+		//이때 안쪽에 조건문을 처리해서 특정 조건에 부합되는 값만 filtering해서 리턴
+		setPost(Post.filter((_, idx) => delIndex !== idx));
+	};
+	const editPost = (editIndex) => {
+		setPost(
+			Post.map((el, idx) => {
+				if (editIndex === idx) el.enableUpdate = true;
+				return el;
+			})
+		);
+	};
+	const editCancle = (editIndex) => {
+		setPost(
+			Post.map((el, idx) => {
+				if (editIndex === idx) el.enableUpdate = false;
+				return el;
+			})
+		);
+	};
+	useEffect(() => {
+		localStorage.setItem('post', JSON.stringify(Post));
+	}, [Post]);
 	return (
 		<Layout title={'Community'}>
 			<section>
 				<div className='inputBox'>
 					<h3>Talk</h3>
-					<div className='write'>
-						<input type='text' placeholder='title' />
-						<textarea cols='30' rows='3' placeholder='content'></textarea>
-					</div>
+					<form className='write'>
+						<input type='text' placeholder='title' ref={refTit} />
+						<textarea cols='30' rows='3' placeholder='content' ref={refCon}></textarea>
+					</form>
 					<div className='btnWrap'>
-						<button type='button' className='cancle blind'>
+						<button type='button' className='cancle blind' onClick={resetPost}>
 							CANCLE
 						</button>
-						<button type='button' className='submit'>
+						<button type='button' className='submit' onClick={creatPost}>
 							WRITE
 						</button>
 					</div>
 				</div>
 				<div className='listBox'>
 					<ul>
-						<li>
-							<strong className='tit'>제목ㅈ목제목ㅈ목제목ㅈ목제목ㅈ목</strong>
-							<p className='desc'>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex quam sequi nemo cum magni numquam voluptatem natus vitae obcaecati nulla
-								delectus, libero nobis incidunt facilis praesentium. Sint nisi tenetur alias!
-							</p>
-							<span className='date'></span>
-							<div className='btnWrap'>
-								<button type='button'>Edit</button>
-								<button type='button'>Delete</button>
-							</div>
-						</li>
-						<li>
-							<strong className='tit'>제목ㅈ목제목ㅈ목제목ㅈ목제목ㅈ목</strong>
-							<p className='desc'>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex quam sequi nemo cum magni numquam voluptatem natus vitae obcaecati nulla
-								delectus, libero nobis incidunt facilis praesentium. Sint nisi tenetur alias!
-							</p>
-							<span className='date'></span>
-							<div className='btnWrap'>
-								<button type='button'>Edit</button>
-								<button type='button'>Delete</button>
-							</div>
-						</li>
-						<li>
-							<strong className='tit'>제목ㅈ목제목ㅈ목제목ㅈ목제목ㅈ목</strong>
-							<p className='desc'>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex quam sequi nemo cum magni numquam voluptatem natus vitae obcaecati nulla
-								delectus, libero nobis incidunt facilis praesentium. Sint nisi tenetur alias!
-							</p>
-							<span className='date'></span>
-							<div className='btnWrap'>
-								<button type='button'>Edit</button>
-								<button type='button'>Delete</button>
-							</div>
-						</li>
+						{Post.map((data, idx) => {
+							const date = JSON.stringify(data.date);
+							const strDate = date ? date.split('T')[0].slice(1) : '';
+							if (data.enableUpdate) {
+								return (
+									<li key={data + idx} className='edit'>
+										<input type='text' className='tit' value={data.title} />
+										<textarea className='desc'>{data.content}</textarea>
+										<div className='btnWrap'>
+											<button type='button' onClick={() => editCancle(idx)}>
+												Cancle
+											</button>
+											<button type='button' onClick={() => deletePost(idx)}>
+												Save
+											</button>
+										</div>
+									</li>
+								);
+							} else {
+								return (
+									<li key={data + idx}>
+										<strong className='tit'>{data.title}</strong>
+										<p className='desc'>{data.content}</p>
+										<span className='date'>{strDate}</span>
+										<div className='btnWrap'>
+											<button type='button' onClick={() => editPost(idx)}>
+												Edit
+											</button>
+											<button type='button' onClick={() => deletePost(idx)}>
+												Delete
+											</button>
+										</div>
+									</li>
+								);
+							}
+						})}
 					</ul>
 				</div>
 			</section>
--- src/components/sub/member/Member.jsx
+++ src/components/sub/member/Member.jsx
@@ -1,11 +1,16 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Member.scss';
+import { useHistory } from 'react-router-dom';
+import { useDebounce } from '../../../hook/useDebounce';
 
 export default function Member() {
+	const history = useHistory();
 	const initVal = useRef({ userid: '', pwd1: '', pwd2: '', email: '', comments: '', edu: '', gender: '', interest: [] });
 	const [Val, setVal] = useState(initVal.current);
 	const [Errs, setErrs] = useState({});
+	//useDebouce 훅의 인수로 특정 state를 전달해서 debouncing이 적용된 새로운 state값 반환받음
+	const DebouncedVal = useDebounce(Val);
 
 	const handleReset = () => {
 		setVal(initVal.current);
@@ -37,7 +42,7 @@ export default function Member() {
 		if (value.interest.length === 0) errs.interest = '관심사를 하나이상 선택하세요.';
 		if (!value.edu) errs.edu = '최종학력을 선택하세요.';
 		if (value.pwd1 !== value.pwd2 || !value.pwd2) errs.pwd2 = '두개의 비밀번호를 같게 입력하세요.';
-		if (value.email) errs.email = '올바른 이메일 형식으로 입력하세요';
+		if (!mail.test(value.email)) errs.email = '올바른 이메일 형식으로 입력하세요';
 		if (!num.test(value.pwd1) || !txt.test(value.pwd1) || !spc.test(value.pwd1) || value.pwd1.length < 5)
 			errs.pwd1 = '비밀번호는 특수문자, 문자, 숫자를 모두포함해서 5글자 이상 입력하세요.';
 
@@ -49,8 +54,13 @@ export default function Member() {
 
 		if (Object.keys(check(Val)).length === 0) {
 			alert('회원가입을 축하합니다.');
+			history.push('/');
 		}
 	};
+	//debounding이 적용된 state를 의존성배열에 등록해서 해당 값으로 check함수 호출
+	useEffect(() => {
+		setErrs(check(DebouncedVal));
+	}, [DebouncedVal]);
 
 	return (
 		<Layout title={'Member'}>
@@ -59,87 +69,110 @@ export default function Member() {
 					<form onSubmit={handleSubmit}>
 						<fieldset>
 							<legend className='h'>회원가입 폼</legend>
-							<div className='boxInput'>
-								<label for='userName' className='tit'>
-									NAME
-								</label>
-								<input type='text' id='userName' name='userName' placeholder='User ID' value={Val.userid} onChange={handleChange} />
-								{Errs.userid && <p>{Errs.userid}</p>}
-							</div>
-							<div className='boxInput'>
-								<label for='userEmail' className='tit'>
-									EMAIL
-								</label>
-								<input type='text' id='userEmail' name='email' placeholder='Email' value={Val.email} onChange={handleChange} />
-							</div>
-							<div className='boxInput'>
-								<label for='pwd1' className='tit'>
-									PASSWORD
-								</label>
-								<input type='password' id='pwd1' name='pwd1' placeholder='Password' value={Val.pwd1} onChange={handleChange} />
-							</div>
-							<div className='boxInput'>
-								<label for='pwd2' className='tit'>
-									RE-PASSWORD
-								</label>
-								<input type='text' id='pwd2' name='pwd2' placeholder='Password' value={Val.pwd2} onChange={handleChange} />
-							</div>
-							<div className='boxInput'>
-								<label for='edu' className='tit'>
-									EDUCATION
-								</label>
-								<div className='groupInput'>
-									<select id='edu' name='edu' onChange={handleChange}>
-										<option value=''>Education</option>
-										<option value='elementary-school'>초등학교 졸업</option>
-										<option value='middle-school'>중학교 졸업</option>
-										<option value='high-school'>고등학교 졸업</option>
-										<option value='college'>대학교 졸업</option>
-									</select>
+							<div className='boxInpt'>
+								<div className='boxInput'>
+									<label for='userid' className='tit'>
+										NAME
+									</label>
+									<input type='text' id='userid' name='userid' placeholder='User ID' value={Val.userid} onChange={handleChange} />
 								</div>
+								{Errs.userid && <p className='error'>{Errs.userid}</p>}
 							</div>
-							<div className='boxInput type'>
-								<strong className='tit'>GENDER</strong>
-								<div className='groupInput'>
-									<div className='custom'>
-										<input type='radio' defaultValue='female' id='female' name='gender' onChange={handleChange} />
-										<label htmlFor='female'>Female</label>
-									</div>
-									<div className='custom'>
-										<input type='radio' defaultValue='male' id='male' name='gender' onChange={handleChange} />
-										<label htmlFor='male'>Male</label>
-									</div>
+							<div className=''>
+								<div className='boxInput'>
+									<label for='userEmail' className='tit'>
+										EMAIL
+									</label>
+									<input type='text' id='userEmail' name='email' placeholder='Email' value={Val.email} onChange={handleChange} />
 								</div>
+								{Errs.email && <p className='error'>{Errs.email}</p>}
 							</div>
-							<div className='boxInput type'>
-								<strong className='tit'>INTERESTING</strong>
-								<div className='groupInput'>
-									<div className='custom'>
-										<input type='checkbox' name='interest' id='sports' defaultValue='sports' />
-										<label htmlFor='sports'>Sports</label>
-									</div>
-									<div className='custom'>
-										<input type='checkbox' name='interest' id='reading' defaultValue='reading' />
-										<label htmlFor='reading'>Reading</label>
-									</div>
-									<div className='custom'>
-										<input type='checkbox' name='interest' id='music' defaultValue='music' />
-										<label htmlFor='music'>Music</label>
-									</div>
-									<div className='custom'>
-										<input type='checkbox' name='interest' id='game' defaultValue='game' />
-										<label htmlFor='game'>Game</label>
+							<div>
+								<div className='boxInput'>
+									<label for='pwd1' className='tit'>
+										PASSWORD
+									</label>
+									<input type='password' id='pwd1' name='pwd1' placeholder='Password' value={Val.pwd1} onChange={handleChange} />
+								</div>
+								{Errs.pwd1 && <p className='error'>{Errs.pwd1}</p>}
+							</div>
+							<div>
+								<div className='boxInput'>
+									<label for='pwd2' className='tit'>
+										RE-PASSWORD
+									</label>
+									<input type='password' id='pwd2' name='pwd2' placeholder='Password' value={Val.pwd2} onChange={handleChange} />
+								</div>
+								{Errs.pwd2 && <p className='error'>{Errs.pwd2}</p>}
+							</div>
+							<div>
+								<div className='boxInput'>
+									<label for='edu' className='tit'>
+										EDUCATION
+									</label>
+									<div className='groupInput'>
+										<select id='edu' name='edu' onChange={handleChange}>
+											<option value=''>Education</option>
+											<option value='elementary-school'>초등학교 졸업</option>
+											<option value='middle-school'>중학교 졸업</option>
+											<option value='high-school'>고등학교 졸업</option>
+											<option value='college'>대학교 졸업</option>
+										</select>
 									</div>
 								</div>
+								{Errs.edu && <p className='error'>{Errs.edu}</p>}
 							</div>
-							<div className='boxInput'>
-								<label htmlFor='comments' className='tit'>
-									COMMENT
-								</label>
-								<textarea name='comments' cols='30' rows='5' placeholder='Leave a comment' onChange={handleChange}></textarea>
+							<div>
+								<div className='boxInput type'>
+									<strong className='tit'>GENDER</strong>
+									<div className='groupInput'>
+										<div className='custom'>
+											<input type='radio' defaultValue='female' id='female' name='gender' onChange={handleChange} />
+											<label htmlFor='female'>Female</label>
+										</div>
+										<div className='custom'>
+											<input type='radio' defaultValue='male' id='male' name='gender' onChange={handleChange} />
+											<label htmlFor='male'>Male</label>
+										</div>
+									</div>
+								</div>
+								{Errs.gender && <p className='error'>{Errs.gender}</p>}
+							</div>
+							<div>
+								<div className='boxInput type'>
+									<strong className='tit'>INTERESTING</strong>
+									<div className='groupInput'>
+										<div className='custom'>
+											<input type='checkbox' name='interest' id='sports' defaultValue='sports' onChange={handleCheck} />
+											<label htmlFor='sports'>Sports</label>
+										</div>
+										<div className='custom'>
+											<input type='checkbox' name='interest' id='reading' defaultValue='reading' onChange={handleCheck} />
+											<label htmlFor='reading'>Reading</label>
+										</div>
+										<div className='custom'>
+											<input type='checkbox' name='interest' id='music' defaultValue='music' onChange={handleCheck} />
+											<label htmlFor='music'>Music</label>
+										</div>
+										<div className='custom'>
+											<input type='checkbox' name='interest' id='game' defaultValue='game' onChange={handleCheck} />
+											<label htmlFor='game'>Game</label>
+										</div>
+									</div>
+								</div>
+								{Errs.interest && <p className='error'>{Errs.interest}</p>}
+							</div>
+							<div>
+								<div className='boxInput'>
+									<label htmlFor='comments' className='tit'>
+										COMMENT
+									</label>
+									<textarea name='comments' cols='30' rows='5' placeholder='Leave a comment' onChange={handleChange}></textarea>
+								</div>
+								{Errs.userid && <p className='error'>{Errs.commnets}</p>}
 							</div>
 							<div className='wrapBtn'>
-								<input type='reset' value='Cancel' />
+								<input type='reset' value='Cancel' onClick={handleReset} />
 								<input type='submit' value='Submit' className='submit' />
 							</div>
 						</fieldset>
