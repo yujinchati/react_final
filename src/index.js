633b12961ac23facad5a2179a39473f7df7ddbfb
yujin <yujin.chati@gmail.com>
2024-01-04 16:50:57 +0900

    redux 삭제

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 
 ReactDOM.render(
 	<BrowserRouter>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
 );
