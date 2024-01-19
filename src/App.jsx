2c5956e9190e3074c6e753012871f31e1f98e36b
yujin <yujin.chati@gmail.com>
2024-01-19 16:52:14 +0900

    header merget

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route } from 'react-router-dom';
 import Department from './components/sub/department/Department';
-import Header from './components/common/header/Header';
 import Footer from './components/common/footer/Footer';
 import './globalStyles/Variables.scss';
 import './globalStyles/Reset.scss';
@@ -11,20 +10,29 @@ import Contact from './components/sub/contact/Contact';
 import Community from './components/sub/community/Community';
 import Member from './components/sub/member/Member';
 import { useMedia } from './hook/useMedia';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Header from './components/common/header/Header';
+import { useState } from 'react';
 function App() {
+	const queryClient = new QueryClient();
+	const [Dark, setDark] = useState(false);
+
 	return (
-		<div className={`wrap  ${useMedia()}`}>
-			<Header />
-			<Route path='/department' component={Department} />
-			<Route path='/youtube' component={Youtube} />
-			<Route path='/detail/:id' component={Detail} />
-			<Route path='/gallery' component={Gallery} />
-			<Route path='/contact' component={Contact} />
-			<Route path='/community' component={Community} />
-			<Route path='/member' component={Member} />
-			<Footer />
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className={`wrap  ${Dark ? 'dark' : ''} ${useMedia()}`}>
+				<Header Dark={Dark} setDark={setDark} />
+				<Route path='/department' component={Department} />
+				<Route path='/youtube' component={Youtube} />
+				<Route path='/detail/:id' component={Detail} />
+				<Route path='/gallery' component={Gallery} />
+				<Route path='/contact' component={Contact} />
+				<Route path='/community' component={Community} />
+				<Route path='/member' component={Member} />
+				<Footer />
+			</div>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
 
